13da58c5e40fb2aebfce37b911f51a39ff00c382
Brenden-Trieu <brenden.trieu@gmail.com>
2025-05-04 23:25:55 -0700

    competed

--- spa.js
+++ spa.js
@@ -1,6 +1,39 @@
 const pageContent = document.querySelector("#pageContent");
 const views = {
-    'home': '#home',
-    'titleChange': '#titleChange',
+    "#home": "views/home.html",
+    "#changeTitle": "views/changeTitle.html",
 };
 
+async function render(path) {
+    const route = views[path] || null;
+    if (!route) {
+        pageContent.innerHTML = `<p>Unable to confirm route</p>`;
+    }
+    try {
+        const response = await fetch(route);
+        if (!response.ok) {
+            throw new Error("error at fetch");
+        }
+        pageContent.innerHTML = await response.text();
+    } catch (e) {
+        pageContent.innerHTML = `<p>error: ${e}</p>`;
+    }
+
+    let buttonFunction = document.querySelector("#confirmChange");
+    if (buttonFunction) {
+        buttonFunction.addEventListener("click", () => {
+            let input = document.querySelector("#titleChanged");
+            if (input) {
+                document.querySelector("#textToBeChanged").innerHTML = input.value;
+            }
+        });
+    }
+}
+
+window.addEventListener("load", () => {
+    render(window.location.hash || "#home");
+});
+
+window.addEventListener("hashchange", () => {
+    render(window.location.hash);
+});
